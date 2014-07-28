d14c95307f02aa443e73a816ee7b9e828ae31985
Francisco Baio Dias <xicombd@gmail.com>
2014-07-29 00:22:45 +0100

    Changed logo animation

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
-setTimeout(function(){
+window.onload=setTimeout(function(){
   document.getElementById("logo").src = "/img/bunny1.png";
-}, 500);
 
-setTimeout(function(){
-  document.getElementById("logo").src = "/img/bunny2.png";
-}, 1000);
+  setTimeout(function(){
+    document.getElementById("logo").src = "/img/bunny2.png";
+  }, 500);
+}, 500);
