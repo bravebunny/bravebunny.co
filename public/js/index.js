3545abf5dc8b9d8bcaff405a6fdbe34f87179b22
Francisco Baio Dias <xicombd@gmail.com>
2014-09-11 10:14:15 +0100

    Bunny opens mouth on mouse over

--- public/js/index.js
+++ public/js/index.js
@@ -5,3 +5,10 @@ window.onload=setTimeout(function(){
     document.getElementById("logo").src = "/img/bunny2.png";
   }, 500);
 }, 500);
+
+function hover(element) {
+  element.setAttribute('src', '/img/bunny1.png');
+}
+function unhover(element) {
+  element.setAttribute('src', '/img/bunny2.png');
+}
